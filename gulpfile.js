5b99bab9961d1864d06112617e69994c008cf97f
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-09-14 09:55:52 -0400

    Project now includes actual gulp-plugin called "gulp-inject as oppossed to the task mentioned in last commit"

--- gulpfile.js
+++ gulpfile.js
@@ -16,13 +16,17 @@ gulp.task('style', function() {
 
 gulp.task('inject', function() {
     var wiredep = require('wiredep').stream,
+        inject = require('gulp-inject'),
+        injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: false}),
+        injectOptions = { ignorePath: '/public'},
         options = {
-            bowerJson  : require('./bower.json'),
-            directory  :  './public/lib',
-            ignorePath : '../../public'
+            bowerJson: require('./bower.json'),
+            directory: './public/lib',
+            ignorePath: '../../public'
         };
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 });
