3f95773d37a215c6af1eb25e800f6fea7b82711e
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-09-15 12:52:02 -0400

    Jade now part of project, test driving for now :)

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express'), 
     app = express(),
-    port = 5000;
+    port = process.env.PORT || 5000;
 
     app.use(express.static('public'));
-    app.use(express.static('src/views'));
+    app.set('views', './src/views');
+    app.set('view engine', 'jade');
 
     app.get('/', function(req, res){
-        res.send('Hello world');
+        res.render('index');
     });
     app.get('/books', function(req, res){
         res.send('Hello books');
