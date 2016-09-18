3b9a0e6602aaf4af1bd217f7b6d01b7b12545d6d
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-09-18 13:38:34 -0400

    Project now includes use of express.Router

--- app.js
+++ app.js
@@ -2,33 +2,47 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 5000;
 
-    app.use(express.static('public'));
-    app.set('views', './src/views');
+    var bookRouter = express.Router();
 
-    app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.set('views', './src/views');
 
-    app.get('/', function(req, res) {
-        res.render('index', {
-            title: 'Hello from render',
-            nav: [{
-                Link: '/Books',
-                Text: 'Books'
-            },{
-                Link: '/Authors',
-                Text: 'Authors']
-            });
+app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+    .get(function(req, res){
+        res.send('Hello Books');
     });
 
-
-    
-    app.get('/books', function(req, res){
-        res.send('Hello books');
+bookRouter.route('/single')
+    .get(function(req, res){
+        res.send('Hello Single Book');
     });
 
-    app.listen(port, function(err){
-        console.log('running server on port ' + port);
-    });
+app.use('/Books', bookRouter);
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        },{
+            Link: '/Authors',
+            Text: 'Authors'
+            }]
+        });
+});
+
+
+
+app.get('/books', function(req, res){
+    res.send('Hello books');
+});
+
+app.listen(port, function(err){
+    console.log('running server on port ' + port);
+});
 
 
 
