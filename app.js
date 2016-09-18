c87cb11b50e64653f1979ad71d9aa5568a6ac60e
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-09-18 13:22:20 -0400

    Project now includes Book and Author routes

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ var express = require('express'),
 
     app.set('view engine', 'ejs');
 
-    app.get('/', function(req, res){
-        res.render('index', {title: 'Hello from render', list: ['a','b']});
+    app.get('/', function(req, res) {
+        res.render(
+            'index', {title: 'Hello from render',
+            nav:[{
+                Link: '/Books',
+                Text: 'Books'
+            },
+            {
+                Link: '/Authors',
+                Text: 'Authors'
+
+            }]
+        });
     });
     app.get('/books', function(req, res){
         res.send('Hello books');
