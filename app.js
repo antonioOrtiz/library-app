8e9a86639713f17d67f6ed0022f6266aa708a19a
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-09-21 10:13:37 -0400

    Right now hardcoded data base for books

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 5000,
+var express = require('express'), //*****
+    app = express(), //*****
+    port = process.env.PORT || 5000, //*****
+    books = [
+        {
+            title: 'Sag Harbor',
+            genre: 'fiction',
+            author: 'Colson Whitehead',
+            read: false
+        },
+        {
+            title: '',
+            genre: '',
+            author: '',
+            read: false
+        },
+        {
+            title: '',
+            genre: '',
+            author: '',
+            read: false
+        }
+        {
+            title: '',
+            genre: '',
+            author: '',
+            read: false
+        }
+    ],
 
-    bookRouter = express.Router();
+    bookRouter = express.Router(); //*****
 
-app.use(express.static('public'));
+app.use(express.static('public')); //*****
 
-app.set('views', './src/views');
+app.set('views', './src/views'); //*****
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //*****
 
 bookRouter.route('/')
     .get(function(req, res) {
@@ -23,21 +49,21 @@ bookRouter.route('/')
             }]
         });
     });
+
 bookRouter.route('/')
     .get(function(req, res) {
         res.send('Hello Books');
     });
 
+app.use('/Books', bookRouter);
+
 bookRouter.route('/single')
     .get(function(req, res) {
         res.send('Hello Single Book');
     });
-    
-app.use('/Books', bookRouter);
 
 
-
-app.get('/', function(req, res) {
+app.get('/', function(req, res) { //*****
     res.render('index', {
         title: 'Hello from render',
         nav: [{
@@ -50,10 +76,10 @@ app.get('/', function(req, res) {
     });
 });
 
-app.get('/books', function(req, res) {
+app.get('/books', function(req, res) { //*****
     res.send('Hello books');
 });
 
-app.listen(port, function(err) {
+app.listen(port, function(err) { //*****
     console.log('running server on port ' + port);
 });
