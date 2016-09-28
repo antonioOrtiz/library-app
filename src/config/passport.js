7b9a95262b81eae0d1619a02e328ce6b6082fbf7
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-09-28 14:47:58 -0400

    using passport.initialize passport.session methods to begin user auth

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,14 +1,17 @@
 var passport = require('passport');
 
 module.exports = function(app) {
-    app.use(passport.inititalize()); //*****
+    app.use(passport.initialize()); //*****
     app.use(passport.session()); //*****    
 
     passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(user, done) {
         done(null, user);
     });
 
-    passport.deserializeUser(function(userId, done) {
-        done(null, user);
-    });
+    require('./strategies/local.strategy')();
+
 };
