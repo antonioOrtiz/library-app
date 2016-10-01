4006347129143c15cbf0b188d69b510253cf53ba
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-10-01 13:47:31 -0400

    wired redirect functionality to bookRouter(s)

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -6,52 +6,58 @@ var express = require('express'),
 
     router = function routerFunc(nav) {
         // body... 
-
-       bookRouter.route('/')
-        .get(function (req, res) {
-            var url =
-                'mongodb://localhost:27017/library-app';
-
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('books');
-
-                collection.find({}).toArray(
-                    function (err, results) {
-                        res.render('bookListView', {
-                            title: 'Books',
-                            nav: nav,
-                            books: results
-                        });
-                    }
-                );
-            });
-
+        bookRouter.use(function(req, res, next) {
+            if (!req.user) {
+                res.redirect('/');
+            }
+            next();
         });
 
-    bookRouter.route('/:id')
-        .get(function (req, res) {
-            var id = new ObjectId(req.params.id);
-            var url =
-                'mongodb://localhost:27017/library-app';
+        bookRouter.route('/')
+            .get(function(req, res) {
+                var url =
+                    'mongodb://localhost:27017/library-app';
 
-            mongodb.connect(url, function (err, db) {
-                var collection = db.collection('books');
+                mongodb.connect(url, function(err, db) {
+                    var collection = db.collection('books');
 
-                collection.findOne({_id: id},
-                    function (err, results) {
-                        res.render('bookView', {
-                            title: 'Books',
-                            nav: nav,
-                            book: results
-                        });
-
-                    }
-                );
+                    collection.find({}).toArray(
+                        function(err, results) {
+                            res.render('bookListView', {
+                                title: 'Books',
+                                nav: nav,
+                                books: results
+                            });
+                        }
+                    );
+                });
 
             });
 
-        });
+        bookRouter.route('/:id')
+            .get(function(req, res) {
+                var id = new ObjectId(req.params.id);
+                var url =
+                    'mongodb://localhost:27017/library-app';
 
-    return bookRouter;
-};
+                mongodb.connect(url, function(err, db) {
+                    var collection = db.collection('books');
+
+                    collection.findOne({ _id: id },
+                        function(err, results) {
+                            res.render('bookView', {
+                                title: 'Books',
+                                nav: nav,
+                                book: results
+                            });
+
+                        }
+                    );
+
+                });
+
+            });
+
+        return bookRouter;
+    };
 module.exports = router;
