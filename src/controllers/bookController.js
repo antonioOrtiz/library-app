838e6c37252d544c443e874c0619560fccbb4270
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-10-03 19:21:36 -0400

    Broke out some functionality from bookRoutes.js to bookController.js to give project more separation of concerns-ish feel :)

---
+++ src/controllers/bookController.js
@@ -0,0 +1,13 @@
+var bookController = function bookControllerHandler(bookService, nav) {
+    // body... 
+    var getIndex = function() {},
+        GetById = function() {};
+
+    return {
+        getIndex: getIndex,
+        GetById: GetById
+    };
+
+};
+
+module.exports = bookController;
