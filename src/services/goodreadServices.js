458406e3097daf700c16089ed84689d3829fad01
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-10-05 18:40:34 -0400

    Wiring goodrread API to libray-app

---
+++ src/services/goodreadServices.js
@@ -0,0 +1,8 @@
+var goodreadService = function goodreadServicesHndlr (argument) {
+    // body... 
+    var getBookId = function getBookIdHndlr(id, cb) {
+        cb(null, { description: 'Our Description'});
+    };
+};
+
+module.exports = goodreadService;
