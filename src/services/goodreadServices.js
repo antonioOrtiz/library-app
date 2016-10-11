0b3cf7948a2b043d5e8f255483a11c8c05421946
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-10-11 13:23:31 -0400

    remove goodreadSerices.js from "library-app"

--- src/services/goodreadServices.js
+++
@@ -1,8 +0,0 @@
-var goodreadService = function goodreadServicesHndlr (argument) {
-    // body... 
-    var getBookId = function getBookIdHndlr(id, cb) {
-        cb(null, { description: 'Our Description'});
-    };
-};
-
-module.exports = goodreadService;
